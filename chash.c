92f67af92918f18f93b84f2e265a4e79a820e07b
Evan Kasky <ekasky25@gmail.com>
2024-03-29 11:23:10 -0400

    Adds functions to open and read file 	- Function to open commands.txt in read mode. Exits the program 	  if a error occurs
    	- Function to read a line from the command.txt file. returns 1
    	  if line is read and 0 if EOF is reached or a error occurs
    
    	- Main function loops through the entire file and prints each
    	  line read

--- chash.c
+++ chash.c
@@ -1,10 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+
+/* Define data structs */
+
+
+/* Function Prototypes */
+FILE* open_file();
+int read_line(FILE* fp, char* buffer, size_t bufferSize);
 
 int main(void) {
 
-	printf("Hello There!\n");
+	const size_t bufferSize = 100;
+	char buffer[bufferSize];
+
+	// Open the commands file in read mode
+	FILE* fp = open_file();
+	
+	
+	// Loop through each line of the file
+	while(read_line(fp, buffer, bufferSize)) {
+		
+		printf("%s", buffer);
+	
+	}
+	
+	
+	// Close the commands.txt file
+	fclose(fp);
 
 	return 0;
 }
+
+
+
+/* Function Definitions */
+
+FILE* open_file() {
+
+	// Attempt to open the file in read mode (commads.txt)
+	FILE* fp = fopen("commands.txt", "r");
+	
+	// If the file cannot be open for found print a error and return a NULL ptr
+	if(fp == NULL) {
+	
+		perror("[ERROR]: Could not open 'commands.txt'");
+		exit(1);
+	
+	}
+	
+	return fp;
+	
+
+}
+
+int read_line(FILE* fp, char* buffer, size_t bufferSize) {
+
+	if( fgets(buffer, bufferSize, fp) != NULL ) {
+		return 1;			// Line read sucessfully
+	}
+	
+	else {
+		return 0;			// EOF encountered or error
+	}
+	
+}
+
