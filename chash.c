edfc5040207603a642422859f18a0b65b8942aa4
Evan Kasky <ekasky25@gmail.com>
2024-03-29 11:57:45 -0400

    Adds function to tokenize string 	- Extracts a line read from commands.txt and extracts the 	  (command, param_one, param_two)

--- chash.c
+++ chash.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Define data structs */
+
+typedef struct Line {
+
+	char* command;
+	char* param_one;
+	char* param_two;
+
+} Line;
 
 
 /* Function Prototypes */
 FILE* open_file();
 int read_line(FILE* fp, char* buffer, size_t bufferSize);
+Line* parse_line(char* buffer);
 
 int main(void) {
 
 	const size_t bufferSize = 100;
 	char buffer[bufferSize];
+	Line* temp_line = NULL;
 
 	// Open the commands file in read mode
 	FILE* fp = open_file();
@@ -20,8 +31,12 @@ int main(void) {
 	// Loop through each line of the file
 	while(read_line(fp, buffer, bufferSize)) {
 		
-		printf("%s", buffer);
-	
+		temp_line = parse_line(buffer);
+		
+		printf("Command: %s\n", temp_line->command);
+		printf("Param One: %s\n", temp_line->param_one);
+		printf("Param Two: %s\n\n", temp_line->param_two);
+		
 	}
 	
 	
@@ -64,4 +79,74 @@ int read_line(FILE* fp, char* buffer, size_t bufferSize) {
 	}
 	
 }
+
+
+Line* parse_line(char* buffer) {
+
+	// Remove the new line character from the end of the line
+	buffer[strcspn(buffer, "\n")] = '\0';					// This line was A.I Generated
+
+	// Allocate memory for the line
+	Line* line = (Line*)malloc(sizeof(Line));
+	
+	// Print a error and exit if there is a memory allocation error
+	if(line == NULL) {
+	
+		perror("[ERROR]: Memory allocation error");
+		exit(1);
+	
+	}
+	
+	// Tokenize the line by commands
+	char* token = strtok(buffer, ",");
+	
+	if(token == NULL) {
+	
+		perror("[ERROR]: Could not tokenize string");
+		exit(1);
+	
+	}
+	
+	// Copy the first token to the command
+	line->command = strdup(token);		// This line used A.I to figure out how to allocate string effincently 
+	
+	
+	// Get the next parameter (token 2)
+	
+	token = strtok(NULL, ",");
+	
+	if(token == NULL) {
+	
+		perror("[ERROR]: Could not tokenize string");
+		exit(1);
+	
+	}
+	
+	// Copy the second token to the parameter_one
+	line->param_one = strdup(token);	// This line used A.I to figure out how to allocate string effincently
+	
+	// Get the next parameter (token 3)
+	
+	token = strtok(NULL, ",");
+	
+	if(token == NULL) {
+	
+		perror("[ERROR]: Could not tokenize string");
+		exit(1);
+	
+	}
+	
+	// Copy the second token to the parameter_one
+	line->param_two = strdup(token);	// This line used A.I to figure out how to allocate string effincently
+	
+	
+	return line;
+
+}
+
+
+
+
+
+
 
