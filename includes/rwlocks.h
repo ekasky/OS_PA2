26fb58188f91bbeaac84cc3bdd8e9129d2384848
Evan Kasky <ekasky25@gmail.com>
2024-04-01 10:49:18 -0400

    Adds rwlock_init function 	- Sets the inital setup for the: 		* Number of reads 		* write lock controller 		* lock controller

--- includes/rwlocks.h
+++ includes/rwlocks.h
@@ -1,4 +1,16 @@
 #ifndef __RW_LOCKS_H__
 #define __RW_LOCKS_H__
 
+#include <semaphore.h>
+
+typedef struct rwlock_t {
+
+	sem_t writeLock;						// Used to control access to critcal section when writing
+	sem_t lock;							// Used to control access to crtical section when reading
+	int readers;							// Keeps track of the number of reads
+
+} rwlock_t;
+
+void rwlock_init(rwlock_t* lock);
+
 #endif
--- src/rwlocks.c
+++ src/rwlocks.c
@@ -1,1 +1,15 @@
+/* 
+
+I used the following link as a guide and resource when writing this code:
+	https://github.com/remzi-arpacidusseau/ostep-code/blob/master/threads-sema/rwlock.c
+ */
+
 #include "../includes/rwlocks.h"
+
+void rwlock_init(rwlock_t* lock) {
+
+	lock->reader = 0;					// Sets the number of reads currently held in the lock to 0
+	Sem_init(&lock->lock, 1);				// Ensures multiple readers can access critcal section concurrently
+	Sem_init(&lock->writeLock, 1);				// Ensures only one writer can access the crtical session at a time
+
+}
