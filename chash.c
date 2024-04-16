eab1a581dfa8b39575fa9f5e3ac611d0c46bfb39
Natalya McKay <zea_64@proton.me>
2024-04-16 14:31:24 -0400

    Basic hashmap functions, untested

--- chash.c
+++ chash.c
@@ -17,6 +17,40 @@ uint32_t jenkins_hash(const char* key) {
 	return hash;
 }
 
+#define MAPLEN 64
+
+typedef struct HashRecord {
+	uint32_t hash;
+	uint32_t salary;
+	struct HashRecord* next;
+	char name[50];
+} HashRecord;
+
+typedef struct HashEntry {
+	rwlock_t rwlock;
+	HashRecord* record;
+} HashEntry;
+
+typedef struct Hashmap {
+	HashEntry table[MAPLEN];
+} Hashmap;
+
+Hashmap hashmap_init() {
+	Hashmap map;
+
+	for (int i = 0; i < MAPLEN; i++) {
+		rwlock_init(&map.table[i].rwlock);
+		map.table[i].record = NULL;
+	}
+
+	return map;
+}
+
+uint32_t hashmap_search(Hashmap* hashmap, const char* name) {
+	uint32_t hash = jenkins_hash(name);
+	// todo
+}
+
 int main() {
 	uint32_t hash = jenkins_hash("The quick brown fox jumps over the lazy dog");
 	printf("%x\n", hash);
