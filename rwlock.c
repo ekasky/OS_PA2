f5327bfd1db15028f1ea77ee6abd9ab4635610f5
Natalya McKay <zea_64@proton.me>
2024-04-16 23:20:47 -0400

    Add locking statistic tracking

--- rwlock.c
+++ rwlock.c
@@ -1,10 +1,14 @@
 #include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 #include "common.h"
 #include "common_threads.h"
+
+_Atomic unsigned long read_acquires = 0;
+_Atomic unsigned long read_releases = 0;
+_Atomic unsigned long write_acquires = 0;
+_Atomic unsigned long write_releases = 0;
 
 typedef struct _rwlock_t {
 	sem_t writelock;
@@ -24,6 +28,9 @@ void rwlock_acquire_readlock(rwlock_t* lock) {
 	if (lock->readers == 1)
 		Sem_wait(&lock->writelock);
 	Sem_post(&lock->lock);
+
+	read_acquires++;
+	puts("READ LOCK ACQUIRED");
 }
 
 void rwlock_release_readlock(rwlock_t* lock) {
@@ -32,12 +39,21 @@ void rwlock_release_readlock(rwlock_t* lock) {
 	if (lock->readers == 0)
 		Sem_post(&lock->writelock);
 	Sem_post(&lock->lock);
+
+	read_releases++;
+	puts("READ LOCK RELEASED");
 }
 
 void rwlock_acquire_writelock(rwlock_t* lock) {
 	Sem_wait(&lock->writelock);
+
+	write_acquires++;
+	puts("WRITE LOCK ACQUIRED");
 }
 
 void rwlock_release_writelock(rwlock_t* lock) {
 	Sem_post(&lock->writelock);
+
+	write_releases++;
+	puts("WRITE LOCK RELEASED");
 }
--- rwlock.h
+++ rwlock.h
@@ -6,6 +6,11 @@ typedef struct _rwlock_t {
 	int readers;
 } rwlock_t;
 
+_Atomic unsigned long read_acquires;
+_Atomic unsigned long read_releases;
+_Atomic unsigned long write_acquires;
+_Atomic unsigned long write_releases;
+
 void rwlock_init(rwlock_t* lock);
 void rwlock_acquire_readlock(rwlock_t* lock);
 void rwlock_release_readlock(rwlock_t* lock);
