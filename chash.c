a0be115255230fe1268fec7db78e0bac1582a8de
Natalya McKay <zea_64@proton.me>
2024-04-16 21:43:29 -0400

    All hashmap functions, and basic (single-threaded) test

--- chash.c
+++ chash.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // https://en.wikipedia.org/wiki/Jenkins_hash_function
 uint32_t jenkins_hash(const char* key) {
@@ -18,12 +19,13 @@ uint32_t jenkins_hash(const char* key) {
 }
 
 #define MAPLEN 64
+#define NAMELEN 49
 
 typedef struct HashRecord {
 	uint32_t hash;
 	uint32_t salary;
 	struct HashRecord* next;
-	char name[50];
+	char name[NAMELEN + 1];
 } HashRecord;
 
 typedef struct HashEntry {
@@ -46,12 +48,85 @@ Hashmap hashmap_init() {
 	return map;
 }
 
-uint32_t hashmap_search(Hashmap* hashmap, const char* name) {
+/// Returns a uint32_t salary or -1 if not found.
+int64_t hashmap_search(Hashmap* hashmap, const char* name) {
 	uint32_t hash = jenkins_hash(name);
-	// todo
+	int index = hash % MAPLEN;
+
+	HashEntry* entry = &hashmap->table[index];
+	rwlock_acquire_readlock(&entry->rwlock);
+
+	const HashRecord* record = entry->record;
+	int64_t return_value = -1;
+	while (record != NULL) {
+		if (record->hash == hash) {
+			return_value = record->salary;
+			break;
+		}
+		record = record->next;
+	}
+
+	rwlock_release_readlock(&entry->rwlock);
+	return return_value;
+}
+
+void hashmap_insert(Hashmap* hashmap, const char* name, uint32_t salary) {
+	uint32_t hash = jenkins_hash(name);
+	int index = hash % MAPLEN;
+
+	HashEntry* entry = &hashmap->table[index];
+	rwlock_acquire_writelock(&entry->rwlock);
+
+	HashRecord** last_ptr = &entry->record;
+	HashRecord* record = *last_ptr;
+	while (1) {
+		if (record == NULL) {
+			HashRecord* new = malloc(sizeof(*new));
+			*new = (HashRecord){.hash = hash, .salary = salary, .next = NULL};
+			strncpy(new->name, name, NAMELEN);
+			*last_ptr = new;
+			break;
+		} else if (record->hash == hash) {
+			record->salary = salary;
+			break;
+		}
+		last_ptr = &record->next;
+		record = *last_ptr;
+	}
+
+	rwlock_release_writelock(&entry->rwlock);
+}
+
+void hashmap_delete(Hashmap* hashmap, const char* name) {
+	uint32_t hash = jenkins_hash(name);
+	int index = hash % MAPLEN;
+
+	HashEntry* entry = &hashmap->table[index];
+	rwlock_acquire_writelock(&entry->rwlock);
+
+	HashRecord** last_ptr = &entry->record;
+	HashRecord* record = *last_ptr;
+	while (record != NULL) {
+		if (record->hash == hash) {
+			*last_ptr = record->next;
+			free(record);
+			break;
+		}
+		last_ptr = &record->next;
+		record = *last_ptr;
+	}
+
+	rwlock_release_writelock(&entry->rwlock);
 }
 
 int main() {
-	uint32_t hash = jenkins_hash("The quick brown fox jumps over the lazy dog");
-	printf("%x\n", hash);
+	Hashmap map = hashmap_init();
+
+	hashmap_insert(&map, "John Doe", 1234);
+	int salary = hashmap_search(&map, "John Doe");
+	printf("%d\n", salary);
+
+	hashmap_delete(&map, "John Doe");
+	salary = hashmap_search(&map, "John Doe");
+	printf("%d\n", salary);
 }
